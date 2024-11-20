752fe9d56ef1e8ce2b48a5734570d71c5f9d6200
lllDavid <imperfectrising5527@gmail.com>
2024-11-20 14:10:14 +0100

    sync

---
+++ 1929. Concatenation of Array/code.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        result.push(nums[i]);
+    }
+    for (let i = 0; i < nums.length; i++) {
+        result.push(nums[i]);
+    }
+    return result;
+
+};
