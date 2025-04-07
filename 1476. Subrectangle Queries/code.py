fad0360fab3837a00cec352ea6870f24ab6bc362
lllDavid <imperfectrising5527@gmail.com>
2025-04-07 22:55:15 +0200

    sync

---
+++ 1476. Subrectangle Queries/code.py
@@ -0,0 +1,14 @@
+class SubrectangleQueries:
+    def __init__(self, rectangle):
+        self.rectangle = rectangle
+
+    def updateSubrectangle(self, row1, col1, row2, col2, newValue):
+        for i in range(row1, row2 + 1):
+            for j in range(col1, col2 + 1):
+                self.rectangle[i][j] = newValue
+
+    def getValue(self, row, col):
+        return self.rectangle[row][col]
+
+
+
