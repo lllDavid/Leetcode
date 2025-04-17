9d1131386bbc2225b78006c683c718718321615d
lllDavid <imperfectrising5527@gmail.com>
2025-04-18 00:58:47 +0200

    sync

---
+++ 3512. Minimum Operations to Make Array Sum Divisible by K/code.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function (nums, k) {
+    let sum = nums.reduce((acc, num) => acc + num, 0);
+    return sum % k;
+};
