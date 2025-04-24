40d4887a739c4dfddbc4791e4b939391d18efb18
lllDavid <imperfectrising5527@gmail.com>
2025-04-24 13:23:41 +0200

    sync

---
+++ 1221. Split a String in Balanced Strings/code.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function (s) {
+    let count = 0; 
+    let balance = 0; 
+
+    for (let char of s) {
+        if (char === 'L') {
+            balance++;
+        } else if (char === 'R') {
+            balance--;
+        }
+
+        if (balance === 0) {
+            count++;
+        }
+    }
+
+    return count;
+};
